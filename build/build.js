01af281da7cd4241a0a4a8de42bed4f1c63ff5f9
Mason Mackaman <masondeanm@aol.com>
2019-09-04 16:07:25 -0400

    Add build code

---
+++ build/build.js
@@ -0,0 +1,14 @@
+const
+	fs = require(`fs`),
+	path = require(`path`),
+	pug = require(`pug`);
+
+process.chdir(path.join(__dirname, `..`));
+[
+	`web-gui`,
+	`httpq`,
+].forEach(docs => {
+	const docsData = JSON.parse(fs.readFileSync(path.join(docs, docs + `.json`)));
+	fs.writeFileSync(path.join(docs, `index.html`), pug.renderFile(path.join(`build`, `build.pug`), {docsData}));
+	console.log(docs);
+});
