8ba7dc296c6e5099e82eaf1624b215ed79acb3c0
Mason Mackaman <masondeanm@aol.com>
2019-10-23 14:36:05 -0400

    Use command line arguments with build

--- build/build.js
+++ build/build.js
@@ -4,10 +4,8 @@ const
 	pug = require(`pug`);
 
 process.chdir(path.join(__dirname, `..`));
-[
-	`pug-client`,
-].forEach(docs => {
+for (let docs of process.argv.slice(2)) {
 	const docsData = require(path.join(`..`, docs, docs + `.json`));
 	fs.writeFileSync(path.join(docs, `index.html`), pug.renderFile(path.join(`build`, `build.pug`), docsData));
 	console.log(docs);
-});
+}
